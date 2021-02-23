f9e4ca8ceb05ef420f2c51130576ff0ad38af0da
奉伟 <llandwind1180@gmail.com>
2021-02-23 23:25:33 +0800

    add hangfiretest job

---
+++ src/LandWind.Blog.BackgroundJobs/Jobs/Hangfire/HangfireTestJob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandWind.Blog.BackgroundJobs.Jobs.Hangfire
+{
+    public class HangfireTestJob : IBackgroundJob
+    {
+        public async Task ExecuteAsync()
+        {
+            Console.WriteLine("定时任务测试"); 
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ src/LandWind.Blog.BackgroundJobs/LandWindBlogBackgroundJobsExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hangfire;
+using LandWind.Blog.BackgroundJobs.Jobs.Hangfire;
+
+namespace LandWind.Blog.BackgroundJobs
+{
+    public static class LandWindBlogBackgroundJobsExtensions
+    {
+        public static void UseHangfireTest(this IServiceProvider service)
+        {
+            var job = service.GetService(typeof(HangfireTestJob));
+
+            //RecurringJob.AddOrUpdate("定时任务测试", () => job.ExecuteAsync(), CronType.Minute());
+        }
+    }
+}
